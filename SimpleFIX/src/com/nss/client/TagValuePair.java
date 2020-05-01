f6be3b36ccdf6b2aaab7a7d17f65747a109f1f07
chandimal1991 <37394684+chandimal1991@users.noreply.github.com>
2020-05-01 14:01:11 +0530

    Initial Commit

---
+++ SimpleFIX/src/com/nss/client/TagValuePair.java
@@ -0,0 +1,34 @@
+package com.nss.client;
+
+public class TagValuePair {
+    private String tagName;
+    private int    tagNum;
+    private String tagValue;
+    
+    public String getTagName() {
+	return tagName;
+    }
+    
+    public void setTagName(final String tagName) {
+	this.tagName = tagName;
+    }
+    
+    public int getTagNum() {
+	return tagNum;
+    }
+    
+    public void setTagNum(final int tagNum) {
+	this.tagNum = tagNum;
+    }
+    
+    public String getTagValue() {
+	return tagValue;
+    }
+    
+    public void setTagValue(final String tagValue) {
+	this.tagValue = tagValue;
+    }
+    
+    
+}
+
